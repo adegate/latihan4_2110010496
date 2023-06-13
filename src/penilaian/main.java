f0c6d1148f1727ee09619893a613e072684dd1e2
adegate <adegati365@gmail.com>
2023-06-13 21:15:39 +0800

    membuat program latihan 4

--- src/penilaian/main.java
+++ src/penilaian/main.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package penilaian;
 
-/**
- *
- * @author aoWAVE
- */
 public class main {
     public static void main(String[] args) {
         
